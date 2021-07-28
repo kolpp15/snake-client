ecb5e45c2bb6fe847a0d929f90d0abc677d577fe
Brian Sohn <kolpp15@gmail.com>
2021-07-28 14:11:06 -0700

    ES6 Shorthand syntax import/export

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const net = require("net");
+const {connect} = require('./client');
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.223.133', // IP address here,
-    port: 50542 // PORT number here,
-  });
-  
-  conn.on("connect", () => {
-    console.log('you are now connected.');
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
